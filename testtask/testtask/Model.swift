e4af9f1dd5f5f42f6557be94f545abdc10533859
yatskovets <yatskovets.d@gmail.com>
2022-03-27 19:29:21 +0300

    initial commit + add model

---
+++ testtask/testtask/Model.swift
@@ -0,0 +1,16 @@
+//
+//  CoctailModel.swift
+//  testtask
+//
+//  Created by Dmitry on 27.03.22.
+//
+
+import Foundation
+
+struct Cocktail: Codable {
+  let cocktailName: String
+}
+
+struct Cocktails: Codable {
+    let cocktails: [Cocktail]
+}
